01ba428bffc4847897005299a51993f4e5d8d1b6
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2022-12-12 20:43:45 -0300

    feat: ajuste e criação de arrayList

---
+++ src/com/javacode/estruturadados/arraylist/Aula12.java
@@ -0,0 +1,9 @@
+package com.javacode.estruturadados.arraylist;
+
+import java.util.ArrayList;
+
+public class Aula12 {	
+	public static void main(String[] args) {
+		ArrayList<String> arrayList = new ArrayList<>();
+	}
+}
